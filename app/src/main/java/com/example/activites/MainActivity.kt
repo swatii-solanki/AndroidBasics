dc1aa096b627689b2feddac3a5bb861eb69571b3
Swati Solanki <solankiswati23@gmail.com>
2024-06-27 21:23:08 +0530

    initial commit

---
+++ app/src/main/java/com/example/activites/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.example.activites
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate: ")
+        setContentView(R.layout.activity_main)
+
+        Log.d("TAG", "First Activity")
+
+        findViewById<Button>(R.id.first_button).setOnClickListener {
+//            startActivity(Intent(this, SecondActivity::class.java))
+            Log.d(TAG, "show dialog button clicked")
+            AlertDialog.Builder(this)
+                .setTitle("This is a dialog")
+                .setMessage("This is a dialog")
+                .setPositiveButton(
+                    android.R.string.yes
+                ) { _, _ ->
+                }
+                .setNegativeButton(android.R.string.no, null)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart: ")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume: ")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause: ")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop: ")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart: ")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy: ")
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
+    }
+}
---
+++ app/src/main/java/com/example/activites/SecondActivity.kt
@@ -0,0 +1,34 @@
+package com.example.activites
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SecondActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_second)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        Log.d("TAG", "Second Activity")
+
+        findViewById<Button>(R.id.second_button).setOnClickListener {
+            startActivity(Intent(this, ThirdActivity::class.java))
+        }
+    }
+}
